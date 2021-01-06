4c7d6adc99936acfac3d48631f096b8103fdef0f
Rodimiro Cerrato E <rodyce@gmail.com>
2021-01-05 23:40:53 -0600

    Do API V3 experiments
    Signed-off-by: Rodimiro Cerrato E <rodyce@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 const axios = require('axios').default;
 const ethers = require("ethers");
+import { EXCHANGE_V3_ABI } from "./abi/exchangeV3Abi";
 
 require('dotenv').config();
 
 //import zksync from "zksync";
 //import { ethers } from "ethers";
-const LOOPRING_REST_HOST = "https://uat2.loopring.io";
+const LOOPRING_REST_HOST = "https://api3.loopring.io";
 const MNEMONIC = process.env.TEST_MNEMONIC;
 
 
@@ -17,7 +18,7 @@ enum Security {
 }
 
 async function querySrvTime(): Promise<number> {
-  const url = `${LOOPRING_REST_HOST}/api/v3/timestamp`;
+  const url = `/api/v3/timestamp`;
   const data = {
     security: Security.NONE
   };
@@ -35,9 +36,111 @@ async function querySrvTime(): Promise<number> {
   return timestamp;
 }
 
+async function getMarketConfigs() {
+  const url = `/api/v3/exchange/markets`;
+  const data = {
+    security: Security.NONE
+  };
+
+  const response = await axios.get(
+    url,
+    {
+      baseURL: LOOPRING_REST_HOST,
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      data
+    });
+
+  console.log(response.data);
+}
+
+async function getTokenConfigs() {
+  const url = `/api/v3/exchange/tokens`;
+  const data = {
+    security: Security.NONE
+  };
+
+  const response = await axios.get(
+    url,
+    {
+      baseURL: LOOPRING_REST_HOST,
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      data
+    });
+
+  console.log(response);
+}
+
+async function getUserBalances() {
+  const url = `/api/v3/user/balances`;
+  const data = {
+    security: Security.NONE,
+    accountId: 1
+  };
+
+  const response = await axios.get(
+    url,
+    {
+      baseURL: LOOPRING_REST_HOST,
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      data
+    });
+
+  console.log(response);
+}
+
+async function queryUserInfo() {
+  const owner = '0x89Ac2c53dD852Fe896176CC18D73384844606247'
+  const url = `/api/v3/account`;
+  const data = {
+    security: Security.NONE,
+  };
+
+  const response = await axios.get(
+    url,
+    {
+      baseURL: LOOPRING_REST_HOST,
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      params: {
+        owner
+      },
+      data
+    });
+
+  console.log(response.data.resultInfo);
+}
+
+async function depositEth() {
+  // TODO
+}
+
 async function main() {
   const srvTime = await querySrvTime();
   console.log(`SRV TIME: ${srvTime}`);
+
+  // console.log('MARKET CONFIGURATIONS')
+  // await getMarketConfigs();
+
+  // console.log('TOKEN CONFIGURATIONS')
+  // await getTokenConfigs();
+
+  try {
+    console.log('USER INFO')
+    await queryUserInfo();
+  } catch (err) {
+    console.log(err.response.data);
+  }
 }
 
 main();
